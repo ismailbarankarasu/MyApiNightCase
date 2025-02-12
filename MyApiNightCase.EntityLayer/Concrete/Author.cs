f26703d9f9883662276d45d46f567af5ae7f5b61
İsmail Baran <ismailbaran04@gmail.com>
2025-02-12 13:12:59 +0300

    entityler yazıldı

---
+++ MyApiNightCase.EntityLayer/Concrete/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiNightCase.EntityLayer.Concrete
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string NameSurname { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
