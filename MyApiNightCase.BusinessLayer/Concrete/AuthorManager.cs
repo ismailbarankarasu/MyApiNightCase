741edbe86e56dd4b0737897c9f9bdb652fc5c567
İsmail Baran <ismailbaran04@gmail.com>
2025-02-12 13:12:37 +0300

    entitylerin imzaları tamamlandı

---
+++ MyApiNightCase.BusinessLayer/Concrete/AuthorManager.cs
@@ -0,0 +1,46 @@
+﻿using MyApiNightCase.BusinessLayer.Abstract;
+using MyApiNightCase.DataAccessLayer.Abstract;
+using MyApiNightCase.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiNightCase.BusinessLayer.Concrete
+{
+    public class AuthorManager : IAuthorService
+    {
+        private readonly IAuthorDal _authorDal;
+
+        public AuthorManager(IAuthorDal authorDal)
+        {
+            _authorDal = authorDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _authorDal.Delete(id);
+        }
+
+        public List<Author> TGetAll()
+        {
+            return _authorDal.GetAll();
+        }
+
+        public Author TGetById(int id)
+        {
+            return _authorDal.GetById(id);
+        }
+
+        public void TInsert(Author entity)
+        {
+            _authorDal.Insert(entity);
+        }
+
+        public void TUpdate(Author entity)
+        {
+            _authorDal.Update(entity);
+        }
+    }
+}
