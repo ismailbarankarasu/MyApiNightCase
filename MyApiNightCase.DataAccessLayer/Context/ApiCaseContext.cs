520c965b627451fca69f0a2482a0f29e37e09320
İsmail Baran <ismailbaran04@gmail.com>
2025-02-14 22:36:56 +0300

    identity eklendi

--- MyApiNightCase.DataAccessLayer/Context/ApiCaseContext.cs
+++ MyApiNightCase.DataAccessLayer/Context/ApiCaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyApiNightCase.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyApiNightCase.DataAccessLayer.Context
 {
-    public class ApiCaseContext: DbContext
+    public class ApiCaseContext: IdentityDbContext<AppUser, AppRole, int>
     {
         public ApiCaseContext(DbContextOptions<ApiCaseContext> options) : base(options)
         {
@@ -18,22 +19,19 @@ namespace MyApiNightCase.DataAccessLayer.Context
         public DbSet<Book> Books{ get; set; }
         public DbSet<Category> Categories{ get; set; }
         public DbSet<Feature> Features{ get; set; }
-        public DbSet<User>Users { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Category - Book (1 Category -> ∞ Books)
             modelBuilder.Entity<Book>()
                 .HasOne(b => b.Category)
                 .WithMany(c => c.Books)
                 .HasForeignKey(b => b.CategoryId)
-                .OnDelete(DeleteBehavior.Cascade); // Eğer kategori silinirse, kitaplar da silinir.
+                .OnDelete(DeleteBehavior.Cascade); 
 
-            // Author - Book (1 Author -> ∞ Books)
             modelBuilder.Entity<Book>()
                 .HasOne(b => b.Author)
                 .WithMany(a => a.Books)
                 .HasForeignKey(b => b.AuthorId)
-                .OnDelete(DeleteBehavior.Cascade); // Eğer yazar silinirse, kitaplar da silinir.
+                .OnDelete(DeleteBehavior.Cascade);
 
             base.OnModelCreating(modelBuilder);
         }
