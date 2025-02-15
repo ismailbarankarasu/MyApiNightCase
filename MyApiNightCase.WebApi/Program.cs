0a2053c29388bd585c174ee5fc15969d5e66e2e5
İsmail Baran <ismailbaran04@gmail.com>
2025-02-15 23:57:32 +0300

    veri tabanı bilgisi appsettings den alındı

--- MyApiNightCase.WebApi/Program.cs
+++ MyApiNightCase.WebApi/Program.cs
@@ -5,6 +5,7 @@ using MyApiNightCase.BusinessLayer.Concrete;
 using MyApiNightCase.DataAccessLayer.Abstract;
 using MyApiNightCase.DataAccessLayer.Context;
 using MyApiNightCase.DataAccessLayer.EntityFramework;
+using MyApiNightCase.EntityLayer.Concrete;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,13 +23,10 @@ builder.Services.AddScoped<IBookDal, EfBookDal>();
 builder.Services.AddScoped<IFeatureService, FeatureManager>();
 builder.Services.AddScoped<IFeatureDal, EfFeatureDal>();
 
-builder.Services.AddScoped<IUserService, UserManager>();
-builder.Services.AddScoped<IUserDal, EfUserDal>();
-
 builder.Services.AddDbContext<ApiCaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<ApiCaseContext>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
