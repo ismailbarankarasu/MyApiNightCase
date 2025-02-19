2321cfa62a373c973de745eaf9d90be4c43619ae
İsmail Baran <ismailbaran04@gmail.com>
2025-02-19 23:58:01 +0300

    admin paneli için areas eklendi

---
+++ MyApiNightCase.WebUI/Areas/Admin/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiNightCase.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize] 
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View();
+        }
+    }
+}
