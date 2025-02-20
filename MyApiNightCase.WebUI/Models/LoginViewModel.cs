198f42820884b9aebc8074957ec360baba007dc9
İsmail Baran <ismailbaran04@gmail.com>
2025-02-20 22:38:40 +0300

    özel modeller eklendi

--- MyApiNightCase.WebUI/Models/LoginViewModel.cs
+++ MyApiNightCase.WebUI/Models/LoginViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace MyApiNightCase.WebUI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApiNightCase.WebUI.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Şifre zorunludur")]
         public string Password { get; set; }
     }
 }
--- MyApiNightCase.WebUI/Models/RegisterViewModel.cs
+++ MyApiNightCase.WebUI/Models/RegisterViewModel.cs
@@ -1,11 +1,24 @@
-﻿namespace MyApiNightCase.WebUI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApiNightCase.WebUI.Models
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Ad alanı zorunludur")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı zorunludur")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email alanı zorunludur")]
+        [EmailAddress(ErrorMessage = "Geçerli bir email adresi giriniz")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı zorunludur")]
+        [MinLength(6, ErrorMessage = "Şifre en az 6 karakter olmalıdır")]
         public string Password { get; set; }
     }
 }
