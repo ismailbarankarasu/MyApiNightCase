73502e04dc575ec23d46411983f7e4e390a8ef7f
İsmail Baran <ismailbaran04@gmail.com>
2025-02-21 23:44:32 +0300

    Default controllerdan index yapıldı oradan async olarak viewcomponent verisi çekildi

--- MyApiNightCase.WebUI/Areas/Admin/Controllers/DefaultController.cs
+++ MyApiNightCase.WebUI/Areas/Admin/Controllers/DefaultController.cs
@@ -1,20 +1,62 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyApiNightCase.WebUI.Dtos;
+using Newtonsoft.Json;
 
 namespace MyApiNightCase.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize] 
+    [Authorize]
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
         {
             if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Login");
             }
 
-            return View();
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                var responseMessage = await client.GetAsync("https://localhost:7104/api/Book/AllBookWithAuthorAndCategory");
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                    var books = JsonConvert.DeserializeObject<List<BookWithAuthorAndCategory>>(jsonData);
+
+                    var pageSize = 5;
+                    var totalItems = books.Count;
+                    var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+                    var pagedBooks = books
+                        .Skip((page - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToList();
+
+                    ViewBag.CurrentPage = page;
+                    ViewBag.TotalPages = totalPages;
+                    ViewBag.TotalItems = totalItems;
+
+                    return View(pagedBooks);
+                }
+
+                TempData["ErrorMessage"] = "Veriler alınamadı. Lütfen daha sonra tekrar deneyiniz.";
+                return View(new List<BookWithAuthorAndCategory>());
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Bir hata oluştu. Lütfen daha sonra tekrar deneyiniz.";
+                return View(new List<BookWithAuthorAndCategory>());
+            }
         }
     }
 }
