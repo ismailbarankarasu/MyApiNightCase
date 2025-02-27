5c1c3be914867a8a1487221172c5224fa71e7310
İsmail Baran <ismailbaran04@gmail.com>
2025-02-27 23:07:35 +0300

    admin layout tamamlandı ve componentlerine ayrıldı

---
+++ MyApiNightCase.WebUI/ViewComponents/_AdminLayout/_AdminLayoutCardComponentPartial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApiNightCase.WebUI.Dtos;
+using Newtonsoft.Json;
+
+namespace MyApiNightCase.WebUI.ViewComponents._AdminLayout
+{
+    public class _AdminLayoutCardComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminLayoutCardComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7104/api/Author/AuthorCount");
+            var responseMessageForBookCount = await client.GetAsync("https://localhost:7104/api/Book/BookCount");
+            var responseMessageForAvgBookPrice = await client.GetAsync("https://localhost:7104/api/Book/AvgBookPrice");
+            var responseMessageForCategoryCount = await client.GetAsync("https://localhost:7104/api/Category/CategoryCount");
+            if (responseMessage.IsSuccessStatusCode && responseMessageForBookCount.IsSuccessStatusCode 
+                && responseMessageForCategoryCount.IsSuccessStatusCode && responseMessageForAvgBookPrice.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<int>(jsonData);
+                ViewBag.AuthorCount = value;
+
+                var jsonDataForBookCount = await responseMessageForBookCount.Content.ReadAsStringAsync();
+                var valueForBookCount = JsonConvert.DeserializeObject<int>(jsonDataForBookCount);
+                ViewBag.BookCount = valueForBookCount;
+
+                var jsonDataForAvgBookPrice = await responseMessageForAvgBookPrice.Content.ReadAsStringAsync();
+                var valueForAvgBookPrice = JsonConvert.DeserializeObject<double>(jsonDataForAvgBookPrice);
+                ViewBag.AvgBookPrice = valueForAvgBookPrice;
+
+                var jsonDataForCategoryCount = await responseMessageForCategoryCount.Content.ReadAsStringAsync();
+                var valueForCategoryCount = JsonConvert.DeserializeObject<int>(jsonDataForCategoryCount);
+                ViewBag.CategoryCount = valueForCategoryCount;
+                return View(value);
+            }
+            return View();
+        }
+    }
+}
---
+++ MyApiNightCase.WebUI/ViewComponents/_AdminLayout/_AdminLayoutHeaderComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyApiNightCase.EntityLayer.Concrete;
+
+namespace MyApiNightCase.WebUI.ViewComponents
+{
+    public class _AdminLayoutHeaderComponentPartial : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _AdminLayoutHeaderComponentPartial(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            if (user != null)
+            {
+                ViewBag.UserName = user.UserName;
+                ViewBag.ImageUrl = user.ImageUrl ?? "~/modernizedashboard/assets/images/profile/user-1.jpg";
+            }
+            return View();
+        }
+    }
+}
